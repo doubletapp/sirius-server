2b3fe3702b09b9fbb6e3c8e5907cd669b086ebb1
olegpro <olegpro@yandex-team.ru>
2018-11-09 23:17:30 +0300

    api skeleton for models

---
+++ api/models.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import uuid as uuid
+from django.db import models
+
+
+class User(models.Model):
+    access_token = models.CharField(max_length=200)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+    def __str__(self):
+        return self.access_token
