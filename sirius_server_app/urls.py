2155ee9601cb2adbbe3e1fcf7a445b10342a1a22
olegpro <olegpro@yandex-team.ru>
2018-11-09 18:53:17 +0300

    urls

---
+++ sirius_server_app/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+from rest_framework import routers
+from django.contrib import admin
+
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^admin/', include(admin.site.urls)),
+]
